808b14f8ebd14337394a9246ae298a81a66c3875
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-06-04 09:40:02 -0400

    Project

--- index.js
+++ index.js
@@ -19,19 +19,59 @@ function displayTeams(teamsArray){
 
         let teamList = document.createElement('li')
         // teamList === <li></li>
+        teamList.textContent = `${team.teamName} || ${team.location}`
+        
+        let likesP = document.createElement('p')
+        likesP.textContent = team.likes
 
-        teamList.textContent = `${team.teamName} || ${team.location}` 
+
+        let btn = document.createElement("button")
+        // btn = <button>Like</button>
+        btn.textContent = "Like"
+        btn.dataset.teamId = team.id
+
+        btn.addEventListener("click", function(e){
+            // grab number of likes from dom
+            let likesNumber = parseInt(e.target.parentElement.textContent[0])
+            likesNumber++
+
+            let teamId = e.target.dataset.teamId
+
+            fetch(`http://localhost:3000/teams/${teamId}`, {
+             method: 'PATCH', // or 'PUT'
+             headers: {'Content-Type': 'application/json'},
+             body: JSON.stringify({likes: likesNumber}),
+            })
+            .then(response => response.json())
+        })
+        
+        likesP.append(btn)
+        
+        
         // teamList === <li>The Bulls</li>
 
+       // let btn = document.createElement("button")
+       //  btn.innerHTML = "Like"
+       //  document.ul.appendChild(btn);
         teamsUl.append(teamList)
+        teamsUl.append(likesP)
+        let teamInfo = document.createElement("ul")
+        let abbreviation = document.createElement("li")
+        abbreviation.innerText = team.abbreviation
+        let location = document.createElement("li")
+        location.innerText = team.location
+        let simpleName = document.createElement("li")
+        simpleName.innerText = team.simpleName
+        teamInfo.append(simpleName)
+        teamInfo.append (location)
+        teamInfo.append(abbreviation)
+        teamInfo.hidden = true
+        teamList.append(teamInfo)
+        teamList.addEventListener("click",function(){
+            teamInfo.hidden = !teamInfo.hidden
+            console.log(team)
+        })
     
     })
-    for (i = 0; i <= teams.length; i++){
-        teams[i].addEventListener("click", function(){
-            const moreInfo = document.createElement(li)
-            moreInfo.textContent = team.teamLocation 
-            moreInfo.textContent= team.teamAbbreviation
-            moreInfo.append()
-        })
-    }
+   
 }
